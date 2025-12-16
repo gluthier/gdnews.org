10fa720870e63e4c1887ccc5eca9de186da6f6f1
Gabriel Luthier <gabriel.luthier@epfl.ch>
2025-12-16 09:32:57 +0100

    Create footer pages

--- src/routes/index.js
+++ src/routes/index.js
@@ -619,6 +619,20 @@ router.get('/promoted/:id', async (req, res, next) => {
     }
 });
 
+// Static pages
+router.get('/about', (req, res) => {
+    res.render('pages/about', { title: 'About' });
+});
+
+router.get('/guidelines', (req, res) => {
+    res.render('pages/guidelines', { title: 'Guidelines' });
+});
+
+router.get('/legal', (req, res) => {
+    res.render('pages/legal', { title: 'Legal' });
+});
+
+
 
 
 module.exports = router;
