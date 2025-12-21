b612428eee3c8aade6532b376618967821941539
Gabriel Luthier <gabriel.luthier@epfl.ch>
2025-12-21 10:42:33 +0100

    Setup pm2 process management

---
+++ ecosystem.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  apps: [
+    {
+      name: 'gdnews-server',
+      script: 'src/server.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      ignore_watch: ['node_modules', 'logs', 'public'],
+      max_memory_restart: '1G',
+      env_development: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    },
+    {
+      name: 'gdnews-batch',
+      script: 'src/batch_server/batch_server.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      ignore_watch: ['node_modules', 'logs', 'public'],
+      max_memory_restart: '1G',
+      env_development: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    }
+  ]
+};
