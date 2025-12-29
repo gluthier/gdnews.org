455e26a47718c1a6a739a0f40885c17a72455970
Gabriel Luthier <gabriel.luthier@epfl.ch>
2025-12-29 23:26:21 +0100

    Add goatcounter analytics

--- src/server.js
+++ src/server.js
@@ -22,10 +22,10 @@ app.use(helmet({
     contentSecurityPolicy: {
         directives: {
             ...helmet.contentSecurityPolicy.getDefaultDirectives(),
-            "script-src": ["'self'", "blob:", "https://js.stripe.com", "https://*.stripe.com"],
-            "img-src": ["'self'", "data:", "https://*.stripe.com"],
+            "script-src": ["'self'", "blob:", "https://js.stripe.com", "https://*.stripe.com", "https://gc.zgo.at"],
+            "img-src": ["'self'", "data:", "https://*.stripe.com", "https://gc.zgo.at"],
             "frame-src": ["'self'", "https://js.stripe.com", "https://hooks.stripe.com", "https://*.stripe.com"],
-            "connect-src": ["'self'", "https://api.stripe.com", "https://*.stripe.com"],
+            "connect-src": ["'self'", "https://api.stripe.com", "https://*.stripe.com", "https://gdnews.goatcounter.com", "https://gc.zgo.at"],
             "form-action": ["'self'", "https://checkout.stripe.com", "https://*.stripe.com"],
             "upgrade-insecure-requests": process.env.NODE_ENV === 'production' ? [] : null
         }
